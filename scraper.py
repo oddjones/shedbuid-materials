dd40020add0e768399e01162c07fd2410be7e46b
Dominic Jones <dom@oddjones.com>
2014-10-29 15:57:38 +0000

    1.1.2.8

--- scraper.py
+++ scraper.py
@@ -23,7 +23,7 @@ for el in root.cssselect("div#property_table_109 table tbody"):
     #kingspan.append Product("Kingspan50",1,"buildingmaterials.co.uk",el.cssselect("td")[2].text,el.cssselect("td")[3].text)
     print x
     print el.cssselect("td")[0].text
-    x++
+    x=x+1
 
 
 #
