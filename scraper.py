39dd9f18946881faee8b22c56c3af53ca922bfaa
Dominic Jones <dom@oddjones.com>
2014-10-30 09:51:44 +0000

    gg

--- scraper.py
+++ scraper.py
@@ -24,8 +24,8 @@ for el in root.cssselect("div#property_table_109 table tr"):
     print lxml.html.tostring(el)
     if x> 0:
         print el.cssselect("td")[0].text
-        print el.cssselect("td")[2].text
-        print el.cssselect("td")[3].text
+        print el.getchildren()
+        #print el.cssselect("td")[3].text
         #kingspan.append(el.cssselect("td")[0].text+"|1|buildingmaterials.co.uk|"+el.cssselect("td")[2].text+"|"+el.cssselect("td")[3].text)
     x=x+1
 
