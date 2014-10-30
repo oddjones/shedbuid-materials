e6a219567c6e897a04487c3cd1a3a77828ad0eec
Dominic Jones <dom@oddjones.com>
2014-10-30 12:14:47 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,4 +14,6 @@ br.set_debug_responses(True)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 br.open(starting_url)
-soup = BeautifulSoup(br.read())
+
+print br.title()
+#soup = BeautifulSoup(br.read())
